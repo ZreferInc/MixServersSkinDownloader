d21856b99c953f241a06a48057811a0df67e1412
Dmitry <81375514+Zrefer@users.noreply.github.com>
2021-12-20 22:29:17 +0300

    v1.0.0

--- MixServersSkinDownloader/MainForm.Designer.cs
+++ MixServersSkinDownloader/MainForm.Designer.cs
@@ -30,14 +30,95 @@ namespace MixServersSkinDownloader
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.NickLabel = new System.Windows.Forms.Label();
+            this.NickTB = new System.Windows.Forms.TextBox();
+            this.SkinRB = new System.Windows.Forms.RadioButton();
+            this.CloakRB = new System.Windows.Forms.RadioButton();
+            this.DwnldTypLabel = new System.Windows.Forms.Label();
+            this.DwnldBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // NickLabel
+            // 
+            this.NickLabel.AutoSize = true;
+            this.NickLabel.Location = new System.Drawing.Point(15, 10);
+            this.NickLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.NickLabel.Name = "NickLabel";
+            this.NickLabel.Size = new System.Drawing.Size(169, 24);
+            this.NickLabel.TabIndex = 0;
+            this.NickLabel.Text = "Никнейм игрока";
+            this.NickLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // NickTB
+            // 
+            this.NickTB.Location = new System.Drawing.Point(15, 45);
+            this.NickTB.Margin = new System.Windows.Forms.Padding(0);
+            this.NickTB.Name = "NickTB";
+            this.NickTB.Size = new System.Drawing.Size(257, 28);
+            this.NickTB.TabIndex = 0;
+            this.NickTB.TextChanged += new System.EventHandler(this.NickTB_TextChanged);
+            this.NickTB.KeyDown += new System.Windows.Forms.KeyEventHandler(this.NickTB_KeyDown);
+            // 
+            // SkinRB
+            // 
+            this.SkinRB.AutoSize = true;
+            this.SkinRB.Checked = true;
+            this.SkinRB.Cursor = System.Windows.Forms.Cursors.Default;
+            this.SkinRB.Location = new System.Drawing.Point(290, 45);
+            this.SkinRB.Margin = new System.Windows.Forms.Padding(0);
+            this.SkinRB.Name = "SkinRB";
+            this.SkinRB.Size = new System.Drawing.Size(76, 28);
+            this.SkinRB.TabIndex = 1;
+            this.SkinRB.TabStop = true;
+            this.SkinRB.Text = "Скин";
+            this.SkinRB.UseVisualStyleBackColor = true;
+            // 
+            // CloakRB
+            // 
+            this.CloakRB.AutoSize = true;
+            this.CloakRB.Location = new System.Drawing.Point(290, 85);
+            this.CloakRB.Margin = new System.Windows.Forms.Padding(0);
+            this.CloakRB.Name = "CloakRB";
+            this.CloakRB.Size = new System.Drawing.Size(78, 28);
+            this.CloakRB.TabIndex = 2;
+            this.CloakRB.Text = "Плащ";
+            this.CloakRB.UseVisualStyleBackColor = true;
+            // 
+            // DwnldTypLabel
+            // 
+            this.DwnldTypLabel.AutoSize = true;
+            this.DwnldTypLabel.Location = new System.Drawing.Point(290, 10);
+            this.DwnldTypLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.DwnldTypLabel.Name = "DwnldTypLabel";
+            this.DwnldTypLabel.Size = new System.Drawing.Size(140, 24);
+            this.DwnldTypLabel.TabIndex = 0;
+            this.DwnldTypLabel.Text = "Тип загрузки";
+            this.DwnldTypLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // DwnldBtn
+            // 
+            this.DwnldBtn.Location = new System.Drawing.Point(15, 85);
+            this.DwnldBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.DwnldBtn.Name = "DwnldBtn";
+            this.DwnldBtn.Size = new System.Drawing.Size(257, 28);
+            this.DwnldBtn.TabIndex = 3;
+            this.DwnldBtn.Text = "Скачать";
+            this.DwnldBtn.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.DwnldBtn.UseVisualStyleBackColor = true;
+            this.DwnldBtn.Click += new System.EventHandler(this.DwnldBtn_Click);
             // 
             // MainForm
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(724, 369);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.12727F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ClientSize = new System.Drawing.Size(452, 126);
+            this.Controls.Add(this.DwnldBtn);
+            this.Controls.Add(this.CloakRB);
+            this.Controls.Add(this.SkinRB);
+            this.Controls.Add(this.NickTB);
+            this.Controls.Add(this.DwnldTypLabel);
+            this.Controls.Add(this.NickLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(5);
@@ -45,10 +126,18 @@ namespace MixServersSkinDownloader
             this.Name = "MainForm";
             this.Text = "Mix-Servers Skin Downloader";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label NickLabel;
+        private System.Windows.Forms.TextBox NickTB;
+        private System.Windows.Forms.RadioButton SkinRB;
+        private System.Windows.Forms.RadioButton CloakRB;
+        private System.Windows.Forms.Label DwnldTypLabel;
+        private System.Windows.Forms.Button DwnldBtn;
     }
 }
 
--- MixServersSkinDownloader/MainForm.cs
+++ MixServersSkinDownloader/MainForm.cs
@@ -1,4 +1,11 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
 
 namespace MixServersSkinDownloader
 {
@@ -8,5 +15,79 @@ namespace MixServersSkinDownloader
         {
             InitializeComponent();
         }
+
+        private void DwnldBtn_Click(object sender, System.EventArgs e) => Download();
+
+        private void NickTB_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) Download();
+        }
+
+        private void NickTB_TextChanged(object sender, System.EventArgs e)
+        {
+            NickTB.Text = Regex.Replace(NickTB.Text, "[\r ]+", "");
+            NickTB.SelectionStart = NickTB.Text.Length;
+            NickTB.SelectionLength = 0;
+        }
+
+        private void Download()
+        {
+            string URI = $"http://files.mix-servers.com/web/{(SkinRB.Checked ? "skins" : "cloaks")}/{NickTB.Text}.png";
+            string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36";
+
+            using HttpClient Client = new();
+            using HttpRequestMessage Message = new(HttpMethod.Get, URI);
+            Message.Headers.UserAgent.ParseAdd(UserAgent);
+            Message.Headers.TryAddWithoutValidation("Upgrade-Insecure-Requests", "1");
+
+            try
+            {
+                HttpResponseMessage Response =
+                    Client.SendAsync(Message, HttpCompletionOption.ResponseContentRead).Result;
+                if (Response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    Error($"{(SkinRB.Checked ? "Скин" : "Плащ")} по этому никнейму не был найден");
+                    return;
+                }
+
+                byte[] ImageBytes = Response.Content.ReadAsByteArrayAsync().Result;
+                using MemoryStream ms = new(ImageBytes);
+
+                SaveFileDialog SaveDialog = new()
+                {
+                    DefaultExt = ".png",
+                    FileName = $"{NickTB.Text}_{(SkinRB.Checked ? "skin" : "cloak")}.png",
+                    Filter = "PNG Files|*.png",
+                    RestoreDirectory = true,
+                    Title = $"Сохранить {(SkinRB.Checked ? "скин" : "плащ")} игрока {NickTB.Text}"
+                };
+                if (SaveDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        Image.FromStream(ms).Save(SaveDialog.FileName, ImageFormat.Png);
+                    }
+                    catch (Exception ie)
+                    {
+                        throw new BadImageFormatException(ie.Message);
+                    }
+                }
+            }
+            catch (AggregateException ae)
+            {
+                if (ae.InnerException is HttpRequestException hre)
+                    Error($"Ошибка скачивания файла с сервера:\n{hre.Message}");
+            }
+            catch (BadImageFormatException bife)
+            {
+                Error($"Был скачан неверный формат изображения с сервера\nСообщение об ошибке:\n{bife.Message}");
+            }
+            catch (Exception e)
+            {
+                Error($"Неизвестная ошибка:\n{e.Message}");
+            }
+        }
+
+        private void Error(string Message) => MessageBox.Show(Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
     }
 }
